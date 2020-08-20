23a594f8df778049462fc85629be0fb9aa95bb8a
steve kang <klongmarch@yahoo.com>
2020-08-20 18:13:19 -0500

    added mongoose DB connection

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var config = require('./config.dev');
+var mongoose = require('mongoose');
 
 //Test the file
 // console.log(config);
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
 
 var createError = require('http-errors');
 var express = require('express');
