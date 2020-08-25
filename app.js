df270bcd76b3d012a46b6c639e06d66f4427c69c
steve kang <klongmarch@yahoo.com>
2020-08-25 15:36:52 -0500

    working with session data

--- app.js
+++ app.js
@@ -71,6 +71,11 @@ passport.deserializeUser(function(user, done){
   done(null, user);
 });
 
+app.use(function(req,res,next){
+  res.locals.session = req.session;
+  next();
+});
+
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
