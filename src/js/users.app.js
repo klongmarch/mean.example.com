efd3e77fc84d79870aa7855450cedf9a6800330c
steve kang <klongmarch@yahoo.com>
2020-08-27 09:34:19 -0500

    Build a View for a Single User

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -108,6 +108,41 @@ function createUser(){
   app.innerHTML=form;
 }
 
+function viewUser(id){
+
+  let uri = `${window.location.origin}/api/users/${id}`;
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', uri);
+
+  xhr.setRequestHeader(
+    'Content-Type',
+    'application/json; charset=UTF-8'
+  );
+
+  xhr.send();
+
+  xhr.onload = function(){
+    let app = document.getElementById('app');
+    let data = JSON.parse(xhr.response);
+    let card = '';
+
+    card = `<div class="card">
+      <div class="card-header clearfix">
+        <h2 class="h3 float-left">${data.user.first_name} ${data.user.last_name}</h2>
+        <div class="float-right">
+          <a href="#edit-${data.user._id}" class="btn btn-primary">Edit</a>
+        </div>
+      </div>
+      <div class="card-body">
+        <div>${data.user.username}</div>
+        <div>${data.user.email}</div>
+      </div>
+    </div>`;
+
+    app.innerHTML = card;
+  }
+}
+
 function postRequest(formId, url){
   let form = document.getElementById(formId);
   form.addEventListener('submit', function(e){
@@ -116,8 +151,40 @@ function postRequest(formId, url){
     let formData = new FormData(form);
     let uri = `${window.location.origin}${url}`;
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', uri);
+    xhr.open('POST', uri);function viewUser(id){
 
+      let uri = `${window.location.origin}/api/users/${id}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', uri);
+    
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+    
+      xhr.send();
+    
+      xhr.onload = function(){
+        let app = document.getElementById('app');
+        let data = JSON.parse(xhr.response);
+        let card = '';
+    
+        card = `<div class="card">
+          <div class="card-header clearfix">
+            <h2 class="h3 float-left">${data.user.first_name} ${data.user.last_name}</h2>
+            <div class="float-right">
+              <a href="#edit-${data.user._id}" class="btn btn-primary">Edit</a>
+            </div>
+          </div>
+          <div class="card-body">
+            <div>${data.user.username}</div>
+            <div>${data.user.email}</div>
+          </div>
+        </div>`;
+    
+        app.innerHTML = card;
+      }
+    }
     xhr.setRequestHeader(
       'Content-Type',
       'application/json; charset=UTF-8'
@@ -154,7 +221,7 @@ var usersApp = (function() {
           break;
     
         case '#view':
-          console.log('VIEW');
+          viewUser(hashArray[1]);
           break;
     
         case '#edit':
