b6020e042f18dd90238404e26ebeb93e18528ae7
steve kang <klongmarch@yahoo.com>
2020-08-27 09:28:52 -0500

    Create a Common Method for Processing Web Forms

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -70,7 +70,7 @@ function createUser(){
           </div>
         </div>
         <div class="card-body">
-          <form id="registrationForm" class="card-body">
+          <form id="createUser" class="card-body">
             <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
 
             <div class="row">
@@ -108,6 +108,38 @@ function createUser(){
   app.innerHTML=form;
 }
 
+function postRequest(formId, url){
+  let form = document.getElementById(formId);
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let formData = new FormData(form);
+    let uri = `${window.location.origin}${url}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    let object = {};
+    formData.forEach(function(value, key){
+      object[key]=value;
+    });
+
+    xhr.send(JSON.stringify(object));
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      if(data.success===true){
+        window.location.href = '/';
+      }else{
+        document.getElementById('formMsg').style.display='block';
+      }
+    }
+  });
+}
+
 var usersApp = (function() {
 
   return {
@@ -118,6 +150,7 @@ var usersApp = (function() {
       switch(hashArray[0]){
         case '#create':
           createUser();
+          postRequest('createUser', '/api/users');
           break;
     
         case '#view':
