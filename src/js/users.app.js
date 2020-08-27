8b670073d33ef2d9a55a025ea378c1e9089db97a
steve kang <klongmarch@yahoo.com>
2020-08-27 10:19:42 -0500

    Create the method for actually deleting the user

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -246,7 +246,7 @@ function deleteView(id){
 
         <div class="text-center">
           <br>
-          <a class="btn btn-lg btn-danger text-white">
+          <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
             Yes delete ${data.user.username}
           </a>
         </div>
@@ -255,6 +255,30 @@ function deleteView(id){
     </div>`;
 
     app.innerHTML = card;
+  }
+}
+
+function deleteUser(id){
+
+  let uri = `${window.location.origin}/api/users/${id}`;
+  let xhr = new XMLHttpRequest();
+  xhr.open('DELETE', uri);
+
+  xhr.setRequestHeader(
+    'Content-Type',
+    'application/json; charset=UTF-8'
+  );
+
+  xhr.send();
+
+  xhr.onload = function(){
+    let data = JSON.parse(xhr.response);
+    if(data.success === true){
+      window.location.hash = '#';
+    }else{
+      alert('Unknown error, the user could not be deleted');
+    }
+
   }
 }
 
@@ -352,6 +376,10 @@ var usersApp = (function() {
           viewUsers();
           break;
       }
+    },
+
+    deleteUser: function(id){
+      deleteUser(id);
     }
   }
 })();
